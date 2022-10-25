711c3d7fcb8dc74ca59d93c83a0fba2eb3212411
Stefan Andrei <andreistefanciprian@gmail.com>
2022-10-25 13:51:47 +1030

    format code

--- main.py
+++ main.py
@@ -12,16 +12,24 @@ class MetricsCollector:
     and not_ready pod ip addresses as prometheus metrics.
     """
 
-    logging.basicConfig(format='%(levelname)s:%(asctime)s:%(message)s', level=logging.INFO)
+    logging.basicConfig(
+        format="%(levelname)s:%(asctime)s:%(message)s", level=logging.INFO
+    )
 
-    def __init__(self, polling_interval_seconds=2, service=None, namespace=None):
+    def __init__(self, polling_interval_seconds=5, service=None, namespace=None):
         self.polling_interval_seconds = polling_interval_seconds
         self.core_api = client.CoreV1Api()
         self._k8s_client_connected = False
         self.service = service
         self.namespace = namespace
-        self.prom_endpoint_addresses_counter = Gauge('srv_ready_pods', 'Number of current pods that are serving traffic to service.')
-        self.prom_not_ready_addresses_counter = Gauge('srv_not_ready_pods', 'Number of current pods that are not serving traffic to service.')
+        self.prom_endpoint_addresses_counter = Gauge(
+            "srv_ready_pods",
+            "Number of current pods that are serving traffic to service.",
+        )
+        self.prom_not_ready_addresses_counter = Gauge(
+            "srv_not_ready_pods",
+            "Number of current pods that are not serving traffic to service.",
+        )
 
     def __time_track(func):
         """
@@ -32,11 +40,12 @@ class MetricsCollector:
             t1 = time.time()
             result = func(*arg, **kw)
             total_time = time.time() - t1
-            print(f'{func.__name__} ran in {total_time} seconds.')
+            print(f"{func.__name__} ran in {total_time} seconds.")
             return result
+
         return wrapper
 
-    # @__time_track 
+    # @__time_track
     def _initialise_client(self):
         """
         Initialise k8s sdk client if not already initialised.
@@ -50,12 +59,12 @@ class MetricsCollector:
                 logging.info(e)
                 raise
             else:
-                logging.debug('Connection to K8s client was established.')
+                logging.debug("Connection to K8s client was established.")
                 self._k8s_client_connected = True
                 return True
         else:
             return True
-        
+
     def collect_metrics_loop(self):
         """Metrics collector loop"""
 
@@ -63,9 +72,9 @@ class MetricsCollector:
             self.get_endpoints()
             time.sleep(self.polling_interval_seconds)
 
-    # @__time_track 
+    # @__time_track
     def get_endpoints(self):
-        """"
+        """
         Returns a list of service endpoints for a namespaced service.
         Returns a list of not ready pod IPs for a namespaced service.
         """
@@ -73,37 +82,39 @@ class MetricsCollector:
         if self._initialise_client():
             try:
                 # Fetch raw status data from the application
-                result = self.core_api.read_namespaced_endpoints(self.service, self.namespace)
+                result = self.core_api.read_namespaced_endpoints(
+                    self.service, self.namespace
+                )
             except Exception as e:
                 logging.debug(e)
             else:
                 endpoint_addresses = 0
                 not_ready_addresses = 0
-                endpoint_addresses_list = []  # used for debug purposes. To be deleted
-                endpoint_addresses_list_counter = 0  # used for debug purposes. To be deleted
-                not_ready_addresses_list = []  # used for debug purposes. To be deleted
-                not_ready_addresses_list_counter = 0  # used for debug purposes. To be deleted
+                dbg_eps_list = []  # tobedeleted
+                dbg_eps_count = 0  # tobedeleted
+                dbg_not_ready_list = []  # tobedeleted
+                dbg_not_ready_count = 0  # tobedeleted
                 # Update Prometheus metrics with application metrics
                 if result.subsets is not None:
                     for i in result.subsets:
                         if i.addresses is not None:
                             for j in i.addresses:
-                                endpoint_addresses_list.append(j.ip)  # used for debug purposes. To be deleted
+                                dbg_eps_list.append(j.ip)  # tobedeleted
                                 endpoint_addresses += 1
                         if i.not_ready_addresses is not None:
                             for j in i.not_ready_addresses:
                                 not_ready_addresses += 1
-                                not_ready_addresses_list.append(j.ip)  # used for debug purposes. To be deleted
-                not_ready_addresses_list_counter = len(not_ready_addresses_list)  # used for debug purposes. To be deleted
-                endpoint_addresses_list_counter = len(endpoint_addresses_list)  # used for debug purposes. To be deleted
+                                dbg_not_ready_list.append(j.ip)  # tobedeleted
+                dbg_not_ready_count = len(dbg_not_ready_list)  # tobedeleted
+                dbg_eps_count = len(dbg_eps_list)  # tobedeleted
                 self.prom_endpoint_addresses_counter.set(endpoint_addresses)
                 self.prom_not_ready_addresses_counter.set(not_ready_addresses)
-                logging.info(f'ready counter: {endpoint_addresses_list_counter}')  # used for debug purposes. To be deleted
-                logging.info(f'ready IPs: {endpoint_addresses_list}')  # used for debug purposes. To be deleted
-                logging.info(f'not ready counter: {not_ready_addresses_list_counter}')  # used for debug purposes. To be deleted
-                logging.info(f'not ready IPs: {not_ready_addresses_list}')  # used for debug purposes. To be deleted
+                logging.info(f"ready count: {dbg_eps_count}")  # tobedeleted
+                logging.info(f"ready IPs: {dbg_eps_list}")  # tobedeleted
+                logging.info(f"not ready count: {dbg_not_ready_count}")  # tobedeleted
+                logging.info(f"not ready IPs: {dbg_not_ready_list}")  # tobedeleted
         else:
-            logging.info('Connection to K8s client failed.')
+            logging.info("Connection to K8s client failed.")
 
 
 def main():
@@ -113,12 +124,12 @@ def main():
     exporter_port = int(os.getenv("EXPORTER_PORT", "9099"))
 
     # config.load_incluster_config()  # inside cluster authentication
-    config.load_kube_config()   # outside cluster authentication
+    config.load_kube_config()  # outside cluster authentication
 
     t = MetricsCollector(service=service_name, namespace=namespace_name)
     start_http_server(exporter_port)
     t.collect_metrics_loop()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
