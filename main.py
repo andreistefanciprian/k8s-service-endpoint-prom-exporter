12ebe3b33a11f3fef0172559eb938b573f8f2760
Stefan Andrei <andreistefanciprian@gmail.com>
2022-10-25 12:02:28 +1030

    add serv and namespace as class params

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from re import T
 from kubernetes import client, config
 import logging
 import time
@@ -13,13 +14,15 @@ class MetricsCollector:
 
     logging.basicConfig(format='%(levelname)s:%(asctime)s:%(message)s', level=logging.INFO)
 
-    def __init__(self):
+    def __init__(self, service=None, namespace=None):
         self.core_api = client.CoreV1Api()
         self._k8s_client_connected = False
         self.endpoint_addresses = []
         self.not_ready_addresses = []
         self.endpoint_addresses_counter = None
         self.not_ready_addresses_counter = None
+        self.service = service
+        self.namespace = namespace
 
     def __time_track(func):
         """
@@ -53,9 +56,9 @@ class MetricsCollector:
                 return True
         else:
             return True
-
+            
     @__time_track 
-    def get_endpoints(self, service_name, namespace):
+    def get_endpoints(self):
         """"
         Returns a list of service endpoints for a namespaced service.
         Returns a list of not ready pod IPs for a namespaced service.
@@ -63,17 +66,12 @@ class MetricsCollector:
 
         if self._initialise_client():
             try:
-                result = self.core_api.read_namespaced_endpoints(service_name, namespace)
-                # list_namespaced_endpoint_slice
-                # read_namespaced_endpoints
-                # list_namespaced_endpoints
-                # list_namespaced_endpoint_slice
+                # Fetch raw status data from the application
+                result = self.core_api.read_namespaced_endpoints(self.service, self.namespace)
             except Exception as e:
                 logging.debug(e)
-                # raise
             else:
-                # print(result.subsets[0].not_ready_addresses)
-                # print(result.subsets[0].addresses[0].ip)
+                # Update Prometheus metrics with application metrics
                 if result.subsets is not None:
                     for i in result.subsets:
                         if i.addresses is not None:
@@ -84,9 +82,7 @@ class MetricsCollector:
                                 self.not_ready_addresses.append(j.ip)
                 self.not_ready_addresses_counter = len(self.not_ready_addresses)
                 self.endpoint_addresses_counter = len(self.endpoint_addresses)
-                logging.info(f'Endpoints for service {service_name}: {self.endpoint_addresses_counter}')
-                logging.info(f'Not ready IPs for service {service_name}: {self.not_ready_addresses_counter}')
-                return self.endpoint_addresses, self.not_ready_addresses, self.not_ready_addresses_counter, self.endpoint_addresses_counter
+                # return self.endpoint_addresses, self.not_ready_addresses, self.not_ready_addresses_counter, self.endpoint_addresses_counter
         else:
             logging.info('Connection to K8s client failed.')
 
@@ -101,8 +97,15 @@ def main():
     config.load_kube_config()   # outside cluster authentication
 
     while True:
-        t = MetricsCollector()
-        t.get_endpoints(service_name, namespace_name)
+        t = MetricsCollector(service=service_name, namespace=namespace_name)
+        t.get_endpoints()
+        t.not_ready_addresses = []
+        print(f'ep counter: {t.endpoint_addresses_counter}')
+        print(f'ep IPs: {t.endpoint_addresses}')
+        print(f'not ready counter: {t.not_ready_addresses_counter}')
+        print(f'not ready IPs: {t.not_ready_addresses}')
+        print()
+        
         time.sleep(sleep_time)
 
 
