bd6cd164dd30cd334a16f36a3585c0476f9401ed
Stefan Andrei <andreistefanciprian@gmail.com>
2022-10-25 13:29:53 +1030

    add prom exporter

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from re import T
 from kubernetes import client, config
 import logging
 import time
-from datetime import datetime, timezone
+from prometheus_client import start_http_server, Gauge
+import os
 
 
 class MetricsCollector:
@@ -14,15 +14,14 @@ class MetricsCollector:
 
     logging.basicConfig(format='%(levelname)s:%(asctime)s:%(message)s', level=logging.INFO)
 
-    def __init__(self, service=None, namespace=None):
+    def __init__(self, polling_interval_seconds=2, service=None, namespace=None):
+        self.polling_interval_seconds = polling_interval_seconds
         self.core_api = client.CoreV1Api()
         self._k8s_client_connected = False
-        self.endpoint_addresses = []
-        self.not_ready_addresses = []
-        self.endpoint_addresses_counter = None
-        self.not_ready_addresses_counter = None
         self.service = service
         self.namespace = namespace
+        self.prom_endpoint_addresses_counter = Gauge('srv_ready_pods', 'Number of current pods that are serving traffic to service.')
+        self.prom_not_ready_addresses_counter = Gauge('srv_not_ready_pods', 'Number of current pods that are not serving traffic to service.')
 
     def __time_track(func):
         """
@@ -56,8 +55,15 @@ class MetricsCollector:
                 return True
         else:
             return True
-            
-    @__time_track 
+        
+    def collect_metrics_loop(self):
+        """Metrics collector loop"""
+
+        while True:
+            self.get_endpoints()
+            time.sleep(self.polling_interval_seconds)
+
+    # @__time_track 
     def get_endpoints(self):
         """"
         Returns a list of service endpoints for a namespaced service.
@@ -71,42 +77,47 @@ class MetricsCollector:
             except Exception as e:
                 logging.debug(e)
             else:
+                endpoint_addresses = 0
+                not_ready_addresses = 0
+                endpoint_addresses_list = []  # used for debug purposes. To be deleted
+                endpoint_addresses_list_counter = 0  # used for debug purposes. To be deleted
+                not_ready_addresses_list = []  # used for debug purposes. To be deleted
+                not_ready_addresses_list_counter = 0  # used for debug purposes. To be deleted
                 # Update Prometheus metrics with application metrics
                 if result.subsets is not None:
                     for i in result.subsets:
                         if i.addresses is not None:
                             for j in i.addresses:
-                                self.endpoint_addresses.append(j.ip)
+                                endpoint_addresses_list.append(j.ip)  # used for debug purposes. To be deleted
+                                endpoint_addresses += 1
                         if i.not_ready_addresses is not None:
                             for j in i.not_ready_addresses:
-                                self.not_ready_addresses.append(j.ip)
-                self.not_ready_addresses_counter = len(self.not_ready_addresses)
-                self.endpoint_addresses_counter = len(self.endpoint_addresses)
-                # return self.endpoint_addresses, self.not_ready_addresses, self.not_ready_addresses_counter, self.endpoint_addresses_counter
+                                not_ready_addresses += 1
+                                not_ready_addresses_list.append(j.ip)  # used for debug purposes. To be deleted
+                not_ready_addresses_list_counter = len(not_ready_addresses_list)  # used for debug purposes. To be deleted
+                endpoint_addresses_list_counter = len(endpoint_addresses_list)  # used for debug purposes. To be deleted
+                self.prom_endpoint_addresses_counter.set(endpoint_addresses)
+                self.prom_not_ready_addresses_counter.set(not_ready_addresses)
+                logging.info(f'ready counter: {endpoint_addresses_list_counter}')  # used for debug purposes. To be deleted
+                logging.info(f'ready IPs: {endpoint_addresses_list}')  # used for debug purposes. To be deleted
+                logging.info(f'not ready counter: {not_ready_addresses_list_counter}')  # used for debug purposes. To be deleted
+                logging.info(f'not ready IPs: {not_ready_addresses_list}')  # used for debug purposes. To be deleted
         else:
             logging.info('Connection to K8s client failed.')
 
 
 def main():
     # define vars
-    sleep_time = 1
     service_name = "istiod-istio-1611"
     namespace_name = "istio-system"
+    exporter_port = int(os.getenv("EXPORTER_PORT", "9099"))
 
     # config.load_incluster_config()  # inside cluster authentication
     config.load_kube_config()   # outside cluster authentication
 
-    while True:
-        t = MetricsCollector(service=service_name, namespace=namespace_name)
-        t.get_endpoints()
-        t.not_ready_addresses = []
-        print(f'ep counter: {t.endpoint_addresses_counter}')
-        print(f'ep IPs: {t.endpoint_addresses}')
-        print(f'not ready counter: {t.not_ready_addresses_counter}')
-        print(f'not ready IPs: {t.not_ready_addresses}')
-        print()
-        
-        time.sleep(sleep_time)
+    t = MetricsCollector(service=service_name, namespace=namespace_name)
+    start_http_server(exporter_port)
+    t.collect_metrics_loop()
 
 
 if __name__ == '__main__':
